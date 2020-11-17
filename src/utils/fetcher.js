2d5d12358be8fa6d5d08a81663c9c8aaecd29f6d
sunyeongchoi <sn0716@naver.com>
2020-11-17 17:54:50 +0900

    배포

--- src/utils/fetcher.js
+++ src/utils/fetcher.js
@@ -2,5 +2,6 @@ import axios from "axios";
 
 const fetcher = (url) =>
   axios.get(url, { withCredentials: true }).then((response) => response.data);
+  // axios.get(url).then((response) => response.data);
 
 export default fetcher;
